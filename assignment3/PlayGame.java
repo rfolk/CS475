e6f165477814ef5600bee39c388a20c6a381efe3
Russell Folk <rfolk@russellfolk.com>
2013-03-01 19:52:19 -0700

    Fixed the coding style to match my preferences.
    I need to keep in mind that if there are errors I removed the {} in for-loops.
    The for-loops do have nested ifs that are only "one level in" but have line
    breaks to make the code easier to read in text editors. If this is an error,
    I'll readd the {} to the code.

--- assignment3/PlayGame.java
+++ assignment3/PlayGame.java
@@ -1,178 +1,178 @@
-import java.util.*; 
+import java.util.*;
 import java.io.*;
 import java.lang.*;
 
-public class PlayGame {
+public class PlayGame
+{
 
-	/* 
-	 * print the possible actions   
-         */
+	/**
+		*	print the possible actions
+		*/
+	public static void printActions ( Vector v )
+	{
+		Iterator< int[] > itSucc = v.iterator () ;
 
-	public static void  printActions(Vector v) {
-		Iterator<int[]> itSucc = v.iterator();
-     
-		for (; itSucc.hasNext(); ) {
-			int[] pair = itSucc.next();
-			System.out.println(pair[0] + ", " + pair[1]);
+		for ( ; itSucc.hasNext () ; )
+		{
+			int [] pair = itSucc.next() ;
+			System.out.println ( pair [ 0 ] + ", " + pair [ 1 ] ) ;
 		}
 	}
 
-	/* 
-	 * testPlay: this test the actions, the    
-	 */
-	
-	public static void testPlay() {
+	/**
+		*	testPlay: this test the actions, the
+		*/
+	public static void testPlay ()
+	{
+		State board = new State () ;
 
-        State board = new State();
-		
-        Vector actions = new Vector<int[]> ();	
+		Vector actions = new Vector< int[] > () ;
 
-        System.out.println(board.toString());        
-        
-        while (board.terminal() == false) {
-        	System.out.println(board.toString());  
-        	System.out.println("Player "+board.getPlayer());  
-        	actions = board.action();
-    		Iterator<int[]> itSucc = actions.iterator();            
-        	printActions(actions);
-        	int[] a = new int[2];
-        	int player = board.getPlayer();
-        	a = itSucc.next();
-        	// play the first available move 
-        	State c = board.result(player, a[0], a[1]);
-		board = c; 
-        }
-        System.out.println("Final ?" + board.terminal());        
-        System.out.println("Value ?" + board.utility());        
-	}
-		
-	public static void testPlayOptimal() {
-		
-        // State  board = new State(); 
+		System.out.println ( board.toString() ) ;
 
-        State board = new State(2,1,0,0,1,0,0,2,0);
-       
-        while (board.terminal() == false) {
-            int[] move = minimax(board); 
-            System.out.println("Player: "+board.getPlayer()+ "\nMove: ["+move[0]+","+move[1]+"] => value " + move[2]);
-            State c = board.result(board.getPlayer(), move[0], move[1]);
-            board = c.copy();
-            System.out.println("Board \n" + board.toString());
-        }
-        System.out.println("Final ?" + board.terminal());        
-        System.out.println("Value ?" + board.utility());        
-	
-	} 
-	/* 
-	 * testAction used for testing the computation of 
-	 * possible actions in a given configuration 
-	 */
-	
-	public static void testAction() {
-
-        State board = new State();
-		
-        Vector actions = new Vector<int[]> ();	
-
-        System.out.println(board.toString());        
-        actions = board.action();        
-        printActions(actions);
-        
-        System.out.println("After setting 1 at 0 0");        
-        board.setAt(1, 0, 0);        
-        actions = board.action();
-        printActions(actions);
-	
-	}
-	/* 
- 	 * Test the utility function 
-	 */
-	
-	public static void testUtility( ) {
-
-        State board = new State(1, 1, 1, 2, 2, 0, 0, 0, 0);
-
-        System.out.println(board.toString());        
-        System.out.println("Final ?" + board.terminal());        
-        System.out.println("Value ?" + board.utility());        
-	
-        board = new State(1, 1, 0, 2, 2, 2, 1, 0, 0);
-        System.out.println(board.toString());        
-        System.out.println("Final ?" + board.terminal());        
-        System.out.println("Value ?" + board.utility());        
-	
-        board = new State(1, 1, 2, 2, 2, 2, 1, 1, 0);
-        System.out.println(board.toString());        
-        System.out.println("Final ?" + board.terminal());        
-        System.out.println("Value ?" + board.utility());        
-
-        board = new State(1, 1, 2, 1, 2, 1, 2, 1, 2);
-        System.out.println(board.toString());        
-        System.out.println("Final ?" + board.terminal());        
-        System.out.println("Value ?" + board.utility());        
-
-	}
-	
-        /*
-           Implement the minimax or the alpha-beta search algorithms 
-        */ 
-
-	/* you should implement the algorithm that will return 
- 	   the move that the player who has the turn should 
-	   play - look for the description of the alpha-beta-algorithm 
-	   implementation in the book of note 
-	   int[] is only the place holder - you can change it into
-	   anything you like       
-	*/ 
-
-	public static int[] minimax(State s) {
- 
+		while ( board.terminal() == false )
+		{
+			System.out.println ( board.toString () ) ;
+			System.out.println ( "Player " + board.getPlayer () ) ;
+			actions = board.action() ;
+			Iterator< int[] > itSucc = actions.iterator () ;
+			printActions( actions ) ;
+			int[] a = new int [ 2 ] ;
+			int player = board.getPlayer () ;
+			a = itSucc.next () ;
+			// play the first available move
+			State c = board.result ( player , a[ 0 ] , a[ 1 ] ) ;
+			board = c ;
+		}
+		System.out.println ( "Final ?" + board.terminal () ) ;
+		System.out.println ( "Value ?" + board.utility () ) ;
 	}
 
-	/* you should implement the minvalue function 
-	   or minvalue for alpha-beta search  - again, 
-	   change int[] to anything that you feel it should be 
-	*/ 
-	
-	public static int[] minvalue(State s) {
-		
- 
+	public static void testPlayOptimal ()
+	{
+		//	State  board = new State();
+
+		State board = new State ( 2 , 1 , 0 , 0 , 1 , 0 , 0 , 2 , 0 ) ;
+
+		while ( board.terminal () == false )
+		{
+			int[] move = minimax ( board ) ;
+			System.out.println ( "Player: " + board.getPlayer () +
+													 "\nMove: [" + move [ 0 ] + "," + move [ 1 ] +
+													 "] => value " + move [ 2 ] ) ;
+			State c = board.result ( board.getPlayer () , move [ 0 ] , move [ 1 ] ) ;
+			board = c.copy () ;
+			System.out.println ( "Board \n" + board.toString () ) ;
+		}
+		System.out.println ( "Final ?" + board.terminal () ) ;
+		System.out.println ( "Value ?" + board.utility () ) ;
 	}
 
-	/* you should implement the maxvalue function 
-	   or maxvalue for alpha-beta search  - again, 
-	   change int[] to anything that you feel it should be 
-	*/ 
+	/**
+		*	testAction used for testing the computation of
+		*	possible actions in a given configuration
+		*/
+	public static void testAction ()
+	{
+		State board = new State () ;
 
-	public static int[] maxvalue(State s) {
+		Vector actions = new Vector<int[]> () ;
 
- 
+		System.out.println ( board.toString () ) ;
+		actions = board.action () ;
+		printActions ( actions ) ;
+
+		System.out.println ( "After setting 1 at 0 0" ) ;
+		board.setAt ( 1 , 0 , 0 ) ;
+		actions = board.action () ;
+		printActions ( actions ) ;
 	}
-	
+
+	/**
+		*	Test the utility function
+		*/
+	public static void testUtility ()
+	{
+		State board = new State ( 1 , 1 , 1 , 2 , 2 , 0 , 0 , 0 , 0 ) ;
+
+		System.out.println ( board.toString () ) ;
+		System.out.println ( "Final ?" + board.terminal () ) ;
+		System.out.println ( "Value ?" + board.utility () ) ;
+
+		board = new State ( 1 , 1 , 0 , 2 , 2 , 2 , 1 , 0 , 0 ) ;
+		System.out.println ( board.toString () ) ;
+		System.out.println ( "Final ?" + board.terminal () ) ;
+		System.out.println ( "Value ?" + board.utility () ) ;
+
+		board = new State ( 1 , 1 , 2 , 2 , 2 , 2 , 1 , 1 , 0 ) ;
+		System.out.println ( board.toString () ) ;
+		System.out.println ( "Final ?" + board.terminal () ) ;
+		System.out.println ( "Value ?" + board.utility () ) ;
+
+		board = new State ( 1 , 1 , 2 , 1 , 2 , 1 , 2 , 1 , 2 ) ;
+		System.out.println ( board.toString () ) ;
+		System.out.println ( "Final ?" + board.terminal () ) ;
+		System.out.println ( "Value ?" + board.utility () ) ;
+	}
+
+	/*
+			Implement the minimax or the alpha-beta search algorithms
+		*/
+
+	/*
+			you should implement the algorithm that will return
+			the move that the player who has the turn should
+			play - look for the description of the alpha-beta-algorithm
+			implementation in the book of note
+			int[] is only the place holder - you can change it into
+			anything you like
+		*/
+	public static int [] minimax ( State s )
+	{
+
+	}
+
+	/*
+			you should implement the minvalue function
+			or minvalue for alpha-beta search  - again,
+			change int[] to anything that you feel it should be
+		*/
+	public static int [] minvalue ( State s )
+	{
+
+	}
+
+	/*
+			you should implement the maxvalue function
+			or maxvalue for alpha-beta search  - again,
+			change int[] to anything that you feel it should be
+		*/
+	public static int [] maxvalue ( State s )
+	{
+
+	}
+
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main ( String [] args )
+	{
+		testAction();
+		testUtility();
+		testPlay();
 
-        
-        testAction(); 
-        testUtility();
-        testPlay();
+		/* it is expected that the following should be able to executed
 
-        /* it is expected that the following should be able to executed 
+		State board = new State();
+		int[] move = minimax(board);
+		System.out.println("Move: ["+move[0]+","+move[1]+"] => value " + move[2]);
 
-        State board = new State();
-        int[] move = minimax(board); 
-        System.out.println("Move: ["+move[0]+","+move[1]+"] => value " + move[2]);
 
-		
-        State board = new State(2,1,1,0,1,0,0,2,0);
-        board.setPlayer(2);
-        int[] move = minvalue(board);
-        System.out.println(board.toString());        
-        System.out.println("Move: ["+move[0]+","+move[1]+"] => value " + move[2]);
- 	*/ 
-
+		State board = new State(2,1,1,0,1,0,0,2,0);
+		board.setPlayer(2);
+		int[] move = minvalue(board);
+		System.out.println(board.toString());
+		System.out.println("Move: ["+move[0]+","+move[1]+"] => value " + move[2]);
+		*/
 	}
-
 }
--- assignment3/State.java
+++ assignment3/State.java
@@ -1,230 +1,243 @@
-/*
- * state node class 
- * this class defines the states and 
- * the functions action, terminal, utility  
- * for the tic-tac-toe game 
- */
+/**
+	*	state node class
+	*	this class defines the states and
+	*	the functions action, terminal, utility
+	*	for the tic-tac-toe game
+	*/
 
-import java.util.*;
+import java.util.* ;
 
-public class State {
+public class State
+{
+	private int [][] content ;	// the configuration of the state
+	private int player ;				// the player who has the move
 
-	private int[][] content;   // the configuration of the state 
-	private int player;        // the player who has the move   
+	/**
+		*	An empty state
+		*/
+	public State ()
+	{
+		content = new int [ 3 ] [ 3 ] ;
+		for ( int i = 0 ; i < 3 ; i ++ )
+			for ( int j = 0 ; j < 3 ; j ++ )
+				content [ i ] [ j ] = 0 ;
+		player = 1 ;
+	}
 
-	
-	public State() {           // an empty state    
-		content = new int[3][3];
-    	for (int i=0; i < 3; i++) 
-    		for (int j=0; j< 3; j++)  
-    			content[i][j] = 0;
-    	player = 1; 
+	/**
+		*	the next constructor defines a configuration of the game
+		*	in the following way:
+		*				a b c
+		*				d e f
+		*				g h i
+		*	assume that each letter stands for a number (1 or 2 or 0)
+		*	with 0 denotes the blank
+		*	1 is for X player (max)
+		*	2 is for o player (min)
+		*	(ignoring wrong numbers!)
+		*/
+	public State ( int a , int b , int c ,
+								 int d , int e , int f ,
+								 int g , int h , int i )
+	{
+		content = { { a , b , c } , { d , e , f } , { g , h , i } } ;
+		/*content = new int [ 3 ] [ 3 ] ;
+		content [ 0 ] [ 0 ] = a ;
+		content [ 0 ] [ 1 ] = b ;
+		content [ 0 ] [ 2 ] = c ;
+		content [ 1 ] [ 0 ] = d ;
+		content [ 1 ] [ 1 ] = e ;
+		content [ 1 ] [ 2 ] = f ;
+		content [ 2 ] [ 0 ] = g ;
+		content [ 2 ] [ 1 ] = h ;
+		content [ 2 ] [ 2 ] = i ;*/
+		player = 1 ;
 	}
-	
-	/*
-	 * the next constructor defines a configuration of the game 
-	 * in the following way:
-	 *      a b c 
-	 *      d e f 
-	 *      g h i
-	 * assume that each letter stands for a number (1 or 2 or 0)  
-	 * with 0 denotes the blank 
-	 * 1 is for X player (max) 
-	 * 2 is for o player (min) 
-         * (ignoring wrong numbers!) 
-	 */
-	
-	public State(int a, int b, int c, int d, int e, int f, int g, int h, int i) {
-		content = new int[3][3];
-		content[0][0] = a; 
-		content[0][1] = b; 
-		content[0][2] = c; 
-		content[1][0] = d; 
-		content[1][1] = e; 
-		content[1][2] = f; 
-		content[2][0] = g; 
-		content[2][1] = h; 
-		content[2][2] = i; 
-    		player = 1; 
+
+	//	return the current player
+	public int getPlayer ()
+	{
+		return player ;
 	}
-	
-	// return the current player 
-	public int getPlayer() {
-		return player; 
+
+	//	set who has the next move
+	public void setPlayer ( int c )
+	{
+		player = c ;
 	}
-	
-	// set who has the next move 
-	
-	public void setPlayer(int c) {
-		player = c;  
-	}
-	
-	// computing the result of an action 
-	// of placing a symbol at the location (x,y) 
-	
-	public State result(int player, int x, int y) {
-		State s = this.copy(); 
-		if (player == 1) {
-			s.setAt(1, x, y);
-			s.setPlayer(2);
+
+	//	computing the result of an action
+	//	of placing a symbol at the location (x,y)
+	public State result ( int player , int x , int y )
+	{
+		State s = this.copy () ;
+		if ( player == 1 )
+		{
+			s.setAt ( 1 , x , y ) ;
+			s.setPlayer ( 2 ) ;
 		}
-		else {
-			s.setAt(2, x, y);
-			s.setPlayer(1);
+		else
+		{
+			s.setAt ( 2 , x , y ) ;
+			s.setPlayer ( 1 ) ;
 		}
-		return s;
+		return s ;
 	}
 
-	// set the value of a square on the board 
-	
-	public void setAt(int value, int x, int y) {
-		content[x][y] = value;
+	//	set the value of a square on the board
+	public void setAt ( int value , int x , int y )
+	{
+		content [ x ] [ y ] = value ;
 	}
-	
-	
-	// copy a state 
-	
-    public State copy() {
-    	State n = new State();
-    	for (int i=0; i < 3; i++) {
-    		for (int j=0; j< 3; j++) { 
-    			n.content[i][j] = content[i][j];
-    		}
-    	}	
-    	n.player = player;
-    	return n; 
-    }
 
-    // checking for equality between two states 
-    public boolean equal(State n) {
-    	for (int i=0; i < 3; i++) 
-    		for (int j=0; j< 3; j++)  
-    			if (n.content[i][j] != content[i][j]) return false;
-    	return true; 
-    }
-    
-    // checking whether a state is a final state of the game 
-    
-    public boolean terminal() {
-    	
-    	int count = 0; 
-   	
-    	// equal row 
-    	for (int i=0; i < 3; i++) {
-    		if (content[i][0] == content[i][1] &&
-    				content[i][0] == content[i][2] && 
-    						content[i][0] != 0
-    				 ) return true;     		
-    	}
-    	
-    	// equal column 
+	//	copy a state
+	public State copy ()
+	{
+		State n = new State () ;
+		for ( int i = 0 ; i < 3 ; i ++ )
+			for ( int j = 0 ; j < 3 ; j ++ )
+				n.content [ i ] [ j ] = content [ i ] [ j ] ;
+		n.player = player ;
+		return n ;
+	}
 
-    	for (int i=0; i < 3; i++) {
-    		if (content[0][i] == content[1][i] &&
-    				content[0][i] == content[2][i]	&& 
-    						content[0][i] != 0
-    				 ) return true;     		
-    	}
-    	
-    	// equal diagonal top left to bottom right
-    	if (content[0][0] == content[1][1] && 
-    			content[0][0] == content[2][2] && 
-						content[0][0] != 0
-   	 				 ) return true;
+	//	checking for equality between two states
+	public boolean equal ( State n )
+	{
+		for ( int i = 0 ; i < 3 ; i ++ )
+			for ( int j = 0 ; j < 3 ; j ++ )
+				if ( n.content [ i ] [ j ] != content [ i ] [ j ] )
+					return false ;
+		return true ;
+	}
 
-    	// equal diagonal top right to bottom left
-       	if (content[2][0] == content[1][1] && 
-    			content[2][0] == content[0][1] && 
-						content[2][0] != 0
-   	 				 ) return true;
+	//	checking whether a state is a final state of the game
+	public boolean terminal ()
+	{
+		int count = 0 ;
 
-    	// all cells are occupied 
-    	
-    	for (int i=0; i < 3; i++) 
-    		for (int j=0; j< 3; j++)  
-    			if (content[i][j] != 0) count ++;
-    	
-    	if (count == 9) return true; 
-    	
-    	return false; 
-    }
-   
-    // computing the utility of a state (from MAX point of view) 
+		//	equal row
+		for ( int i = 0 ; i < 3 ; i ++ )
+			if ( content [ i ] [ 0 ] == content [ i ] [ 1 ] &&
+					 content [ i ] [ 0 ] == content [ i ] [ 2 ] &&
+					 content [ i ] [ 0 ] != 0 )
+				return true;
 
-    public int utility() {
-    	
-    	int count = 0; 
-   	
-    	// equal row 
-    	for (int i=0; i < 3; i++) {
-    		if (content[i][0] == content[i][1] &&
-    				content[i][0] == content[i][2] && 
-    						content[i][0] != 0
-    				 ) {
-    			if (content[i][0] == 1) return 1;
-    			return -1;     		
-    		}
-    	}
-    	
-    	// equal column 
+		//equal column
+		for ( int i = 0 ; i < 3 ; i ++ )
+			if ( content [ 0 ] [ i ] == content [ 1 ] [ i ] &&
+					 content [ 0 ] [ i ] == content [ 2 ] [ i ] &&
+					 content[0][i] != 0 )
+				return true ;
 
-    	for (int i=0; i < 3; i++) {
-    		if (content[0][i] == content[1][i] &&
-    				content[0][i] == content[2][i]	&& 
-    						content[0][i] != 0
-    				 ) {
-    			if (content[0][i] == 1) return 1;
-    			return -1;     		
-    		} 
-    	}
-    	
-    	// equal diagonal top left to bottom right  
-    	if (content[0][0] == content[1][1] && 
-    			content[0][0] == content[2][2] && 
-						content[0][0] != 0
-   	 				 ) {
-			if (content[0][0] == 1) return 1;
-			return -1;     		
-		} 
+		//	equal diagonal top left to bottom right
+		if ( content [ 0 ] [ 0 ] == content [ 1 ] [ 1 ] &&
+				 content [ 0 ] [ 0 ] == content [ 2 ] [ 2 ] &&
+				 content [ 0 ] [ 0 ] != 0 )
+			return true ;
 
-    	// equal diagonal top right to bottom left
-       	if (content[0][2] == content[1][1] && 
-    			content[0][2] == content[2][0] && 
-						content[0][2] != 0
-   	 				 ) {
-			if (content[2][0] == 1) return 1;
-			return -1;     		
-		}  
-    	
-    	return 0;     		
-		
-    }
-   
-    // identifying the eligible moves for the current state 
-    // returns a vector of pairs of integers, each pair specifies 
-    // a location where the player can mark 
+		//	equal diagonal top right to bottom left
+		if ( content [ 2 ] [ 0 ] == content [ 1 ] [ 1 ] &&
+				 content [ 2 ] [ 0 ] == content [ 0 ] [ 1 ] &&
+				 content [ 2 ] [ 0 ] != 0 )
+			return true ;
 
-    public Vector action() {
-    	Vector actions = new Vector<int[]> ();
+		//	all cells are occupied
+		for ( int i = 0 ; i < 3 ; i ++ )
+			for ( int j = 0 ; j < 3 ; j ++ )
+				if ( content [ i ] [ j ] != 0 )
+					count ++ ;
 
-    	for (int i=0; i < 3; i++) 
-    		for (int j=0; j< 3; j++) { 
-    			int[] a = new int[2];
-    			if (content[i][j] == 0) {
-    				a[0] = i; 
-    				a[1] = j; 
-    				actions.add(a);
-    			}
-    		}
-    	
-    	return actions; 
-    }
-    
-    // display the content of a state 
-    
-    public String toString() {
-    	return " "+ content[0][0]+ " "+ content[0][1]+" "+ content[0][2]+"\n"+ 
-    	       " "+ content[1][0]+ " "+ content[1][1]+" "+ content[1][2]+"\n"+ 
-    	       " "+ content[2][0]+ " "+ content[2][1]+" "+ content[2][2]+"\n"; 
-    }
+			if ( count == 9 )
+				return true ;
+
+		return false;
+	}
+
+	//	computing the utility of a state (from MAX point of view)
+	public int utility ()
+	{
+		int count = 0 ;
+
+		// equal row
+		for ( int i = 0 ; i < 3 ; i ++ )
+		{
+			if ( content [ i ] [ 0 ] == content [ i ] [ 1 ] &&
+					 content [ i ] [ 0 ] == content [ i ] [ 2 ] &&
+					 content [ i ] [ 0 ] != 0 )
+			{
+				if ( content [ i ] [ 0 ] == 1 )
+					return 1 ;
+				return -1 ;
+			}
+		}
+
+		//	equal column
+		for ( int i = 0 ; i < 3 ; i ++ )
+		{
+			if ( content [ 0 ] [ i ] == content [ 1 ] [ i ] &&
+					 content [ 0 ] [ i ] == content [ 2 ] [ i ]	&&
+					 content [ 0 ] [ i ] != 0 )
+			{
+				if ( content [ 0 ] [ i ] == 1 )
+					return 1 ;
+				return -1 ;
+			}
+		}
+
+		// equal diagonal top left to bottom right
+		if ( content [ 0 ] [ 0 ] == content [ 1 ] [ 1 ] &&
+				 content [ 0 ] [ 0 ] == content [ 2 ] [ 2 ] &&
+				 content [ 0 ] [ 0 ] != 0 )
+		{
+			if ( content [ 0 ] [ 0 ] == 1 )
+				return 1 ;
+			return -1 ;
+		}
+
+		//	equal diagonal top right to bottom left
+		if ( content [ 0 ] [ 2 ] == content [ 1 ] [ 1 ] &&
+				 content [ 0 ] [ 2 ] == content [ 2 ] [ 0 ] &&
+				 content [ 0 ] [ 2 ] != 0 )
+		{
+			if ( content [ 2 ] [ 0 ] == 1 )
+				return 1 ;
+			return -1 ;
+		}
+
+		return 0;
+	}
+
+		//	identifying the eligible moves for the current state
+		//	returns a vector of pairs of integers, each pair specifies
+		//	a location where the player can mark
+		public Vector action ()
+		{
+			Vector actions = new Vector< int[] > () ;
+
+			for ( int i = 0 ; i < 3 ; i ++ )
+				for ( int j = 0 ; j < 3 ; j ++)
+				{
+					int [] a = new int [ 2 ] ;
+					if ( content [ i ] [ j ] == 0 )
+					{
+						a [ 0 ] = i ;
+						a [ 1 ] = j ;
+						actions.add ( a ) ;
+					}
+				}
+
+			return actions ;
+		}
+
+		//	display the content of a state
+		public String toString()
+		{
+			return " " + content [ 0 ] [ 0 ] + " " + content [ 0 ] [ 1 ] +
+						 " " + content [ 0 ] [ 2 ] + "\n" +
+						 " " + content [ 1 ] [ 0 ] + " " + content [ 1 ] [ 1 ] +
+						 " " + content [ 1 ] [ 2 ] + "\n" +
+						 " " + content [ 2 ] [ 0 ] + " " + content [ 2 ] [ 1 ] +
+						 " " + content [ 2 ] [ 2 ] + "\n" ;
+		}
 }
