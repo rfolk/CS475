e388293ae52758c979e2c63432c4ff748e4bc7be
Russell Folk <rfolk@russellfolk.com>
2013-04-06 16:54:59 -0600

    Fixed spacing.
    So far behind on this project, heaven help me!

--- assignment4/reader.java
+++ assignment4/reader.java
@@ -1,47 +1,56 @@
-import java.io.*;
-import java.util.*;
-import java.lang.*;
- 
-public class reader {
+import java.io.* ;
+import java.util.* ;
+import java.lang.* ;
 
- 	private Vector stores = new Vector<String>();  
- 
-	public reader(String fname) {
+public class reader
+{
 
-		Scanner inFile = null;   
-		
-		try {
-			inFile = new Scanner (new File(fname));		
-			String s; 
-			while (inFile.hasNextLine()) {
-				s = inFile.nextLine();
-				stores.addElement(s);
+	private Vector stores = new Vector<String> () ;
+
+	public reader ( String fname )
+	{
+
+		Scanner inFile = null ;
+
+		try
+		{
+			inFile = new Scanner ( new File ( fname ) ) ;
+			String s ;
+			while ( inFile.hasNextLine () )
+			{
+				s = inFile.nextLine () ;
+				stores.addElement ( s ) ;
 			}
-		} 
-		catch (IOException e){
-		    // If another exception is generated, print a stack trace	
-	            e.printStackTrace();
-	        }
-		finally {
-			if (inFile != null) inFile.close();
 		}
-    	
-	}   	
-	
-	public Vector<String> getStore() {
-		return stores;
+		catch ( IOException e )
+		{
+			// If another exception is generated, print a stack trace
+			e.printStackTrace () ;
+		}
+		finally
+		{
+			if ( inFile != null )
+				inFile.close () ;
+		}
+
 	}
-	
-	public void printProblem() {
-		
-   	 	Iterator<String> itStore = stores.iterator();
-     
-   	 	for (; itStore.hasNext(); ) {
-   	 		String item = itStore.next();
-   	 		System.out.println(item);
-     	}
-		
+
+	public Vector<String> getStore ()
+	{
+		return stores ;
 	}
-	
-	
+
+	public void printProblem ()
+	{
+
+			Iterator<String> itStore = stores.iterator () ;
+
+			for ( ; itStore.hasNext () ; )
+			{
+				String item = itStore.next () ;
+				System.out.println ( item ) ;
+			}
+
+	}
+
 }
--- assignment4/solver.java
+++ assignment4/solver.java
@@ -1,16 +1,17 @@
 
-public class solver {
+public class solver
+{
 
 	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-
-        reader r = new reader(args[0]);
-        r.printProblem();
-        stores cs = new stores(r);
-        cs.printStores();
-        System.out.println("Well defined:  " + cs.well_defined());
+		* @param args
+		*/
+	public static void main ( String [] args )
+	{
+		reader r = new reader ( args [ 0 ] ) ;
+		r.printProblem () ;
+		stores cs = new stores ( r ) ;
+		cs.printStores () ;
+		System.out.println ( "Well defined: " + cs.well_defined () ) ;
 	}
 
 }
